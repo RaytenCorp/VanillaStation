ce3076fecc719ded39c6dbdf54d93de706eada52
TFMB <mail@vanilla-station.ru>
2025-01-31 16:47:50 +0300

    oopsy-doopsy

--- Content.Shared/Vanilla/Bureaucracy/BureaucracyPenComponent.cs
+++ Content.Shared/Vanilla/Bureaucracy/BureaucracyPenComponent.cs
@@ -2,7 +2,7 @@ using Robust.Client.GameObjects;
 using Robust.Shared.GameObjects;
 using Robust.Shared.IoC;
 
-namespace Content.Client.Vanilla.Bureaucracy
+namespace Content.Shared.Vanilla.Bureaucracy
 {
     [RegisterComponent]
     public sealed partial class BureaucracyPenComponent : Component
