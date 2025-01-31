ac55bbb9eb7cb55f7717e28eb3271547b95e79c2
TFMB <mail@vanilla-station.ru>
2025-01-31 15:42:43 +0300

    бюрократические приколямбы

---
+++ Content.Client/Vanilla/Bureaucracy/Components/BureaucracyPenComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Client.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Content.Client.Vanilla.Bureaucracy
+{
+    [RegisterComponent]
+    public sealed partial class BureaucracyPenComponent : Component
+    {
+
+    }
+}
